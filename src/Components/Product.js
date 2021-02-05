75a467b2497103e9a862f1c94b72b2afac7018a7
Santanu Roy <edu.santanu12@gmail.com>
2021-02-06 01:01:27 +0530

    3rd commit

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,5 +1,7 @@
 import React, { Component, useState } from 'react';
 
+var env = process.env.NODE_ENV ;
+
 
 function Product(props) {
 
@@ -41,7 +43,7 @@ function Product(props) {
         <div className="Productdiv">
 
             <div className="prodimgdiv">
-                <img className="productImg" style={{ height: "20vh" }} src={process.env.PUBLIC_URL+"/"+ImageURL} />
+                <img className="productImg" style={{ height: "20vh" }} src={env == "production"?process.env.PUBLIC_URL+"/ecom/"+ImageURL:process.env.PUBLIC_URL+"/"+ImageURL} />
                 <p>{OfferText}</p>
             </div>
             <div className="proddtlsdiv">
--- src/Controllers/controller.js
+++ src/Controllers/controller.js
@@ -1,4 +1,10 @@
 var fetchproducts = ()=>{
+
+    var env = process.env.NODE_ENV ;
+
+    if(env=="production"){
+        return fetch(`${process.env.PUBLIC_URL}/ecom/Schema/ProductSchema.json`)
+    }
     return fetch(`${process.env.PUBLIC_URL}/Schema/ProductSchema.json`)
 };
 
